72911907b332ba61d5f8a25175ad6a53e5b810df
James H. Thorpe <james.thorpe@utexas.edu>
2022-05-06 15:30:28 -0400

    added libjdef.h updates

--- C++/libjdef.h
+++ C++/libjdef.h
@@ -1,16 +1,20 @@
-/*--------------------------------------------------------
-  libjdef.h
-	JHT, Febuary 4, 2022 : created
-
-  .h file which defines the OpenMP, MPI, and other 
-  things for compiling libj 
---------------------------------------------------------*/
-
-//OpenMP is easy
-#if defined (_OPNEMP)
-  #define OMP 1
+//PARALLELIZATION DEFINITIONS
+#define LIBJ_MPI 1
+#if defined (_OPENMP)
+  #define LIBJ_OMP 1
 #endif
 
-//We must manually set if we want MPI
-// If you do not want MPI, comment-out the following line
-#define MPI 1
+//CACHE SIZE DEFINITIONS
+#define L1_BYTES 32768
+#define L2_BYTES 262144
+#define LINE_BYTES 64
+
+//ALIGNMENT DEFINITIONS
+#define DOUBLE_ALIGN 32
+#define FLOAT_ALIGN 32
+#define LONG_ALIGN 32
+#define INT_ALIGN 32
+#define MAX_ALIGN DOUBLE_ALIGN
+
+//compiler specific definitions
+#define LIBJ_RESTRICT __restrict__
